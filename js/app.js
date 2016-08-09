2fb61a40022026fa016c608d69103a16885290b7
lomlo <michael.lo@live.co.uk>
2016-08-09 10:58:29 +0100

    corrected the margin of the figures using the normalize css

--- js/app.js
+++ js/app.js
@@ -1,8 +1,6 @@
 $(document).ready(function() {
- 
     $('.menus h3').on('click', function(e) {
         $(this).next('ul').toggleClass('open');
         e.preventDefault(); return false;
     });
- 
 });
